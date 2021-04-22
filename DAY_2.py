a10c310269a658e660d8386b68fc6705d23d9470
Divya <66710593+Divya171@users.noreply.github.com>
2021-04-22 19:23:22 +0530

    DAY 2 - variables, data types, print

---
+++ DAY_2.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[10]:
+
+
+# understanding variables and print function
+
+city = 'Nagpur'
+temp = 45.6
+hasmetro = 'Yes'
+
+print("My hometown is {}.\nLast year, the average temperatue was approximately {} during summer.\nDoes it have Metro Train ?: {}!".format(city,temp,hasmetro))
+
